eafd20a65e9b8b9d7b569d2ce5574307512bc890
Daniel Tré <daniel.tre@gmail.com>
2020-06-10 20:02:31 -0300

    Create Tree.insert

--- radex.py
+++ radex.py
@@ -8,6 +8,20 @@ class Tree:
     def __init__(self):
         self.root = Node()
 
+    def insert(self, word, node=None):
+        if len(word) < 1:
+            return None
+
+        if node is None:
+            node = self.root
+
+        c = word[0]
+        if c in node.children:
+            return self.insert(word[1:], node.children[c])
+        else:
+            node.children.update({c: Node(c)})
+            return self.insert(word[1:], node.children[c])
+
 
 f = open('./geohash-list.txt', 'r')
 hashes = f.read().split(' ')
