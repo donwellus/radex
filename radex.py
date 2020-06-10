39249237231f5257260e64e77981f651855ac259
Daniel Tré <daniel.tre@gmail.com>
2020-06-10 20:47:55 -0300

    Create Tree.regex

--- radex.py
+++ radex.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Node:
     def __init__(self, value=''):
         self.value = value
@@ -21,6 +24,25 @@ class Tree:
         else:
             node.children.update({c: Node(c)})
             return self.insert(word[1:], node.children[c])
+
+    def regex(self, node=None):
+        if node is None:
+            node = self.root
+
+        if len(node.children) < 1:
+            return node.value
+
+        children = []
+        for c in node.children:
+            children.append(self.regex(node.children[c]))
+
+        if reduce((lambda x, y: x or y), ['[' in s for s in children]):
+            re = '|'.join(children)
+            re = '(' + re + ')'
+        else:
+            re = ''.join(children)
+            re = '[' + re + ']'
+        return node.value + re
 
 
 f = open('./geohash-list.txt', 'r')
